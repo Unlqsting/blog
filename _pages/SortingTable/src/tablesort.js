88b250783bb932bbfb0977bad2f5c71679c0c7ed
Unlqsting <Unlqsting@users.noreply.github.com>
2022-09-28 01:11:56 +0000

    deploy: 828641681244b89a058de199cb8e1be99e1afe31

--- _pages/SortingTable/src/tablesort.js
+++ _pages/SortingTable/src/tablesort.js
@@ -1,34 +1,11 @@
-function sortTable() {
-    var table, rows, switching, i, x, y, shouldSwitch;
-    table = document.getElementById("myTable");
-    switching = true;
-    /* Make a loop that will continue until
-    no switching has been done: */
-    while (switching) {
-      // Start by saying: no switching is done:
-      switching = false;
-      rows = table.rows;
-      /* Loop through all table rows (except the
-      first, which contains table headers): */
-      for (i = 1; i < (rows.length - 1); i++) {
-        // Start by saying there should be no switching:
-        shouldSwitch = false;
-        /* Get the two elements you want to compare,
-        one from current row and one from the next: */
-        x = rows[i].getElementsByTagName("TD")[0];
-        y = rows[i + 1].getElementsByTagName("TD")[0];
-        // Check if the two rows should switch place:
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      }
-      if (shouldSwitch) {
-        /* If a switch has been marked, make the switch
-        and mark that a switch has been done: */
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
-      }
-    }
-  }
+let sortDirection = false;
+let tableData = [
+    { Week: '0', name: 'Tool Setup Sprint and Pair Programming'}
+    { Week: '1', name: 'Introduction to Python'}
+    { Week: '2', name: 'Data Abstraction in Python'}
+    { Week: '3', name: 'Creative Development Sprint'}
+    { Week: '4', name: 'Python Web Server Project'}
+    { Week: '5', name: 'UI Starters'}
+    { Week: '6', name: 'Deployment'}
+
+];
