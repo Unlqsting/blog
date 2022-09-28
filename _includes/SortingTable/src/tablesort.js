92e9b080d2bd5537bdce534f1b21ad01da672e93
Unlqsting <noorbijapur500@gmail.com>
2022-09-27 22:48:07 -0700

    upd

---
+++ _includes/SortingTable/src/tablesort.js
@@ -0,0 +1,11 @@
+let sortDirection = false;
+let tableData = [
+    { Week: '0', name: 'Tool Setup Sprint and Pair Programming'}
+    { Week: '1', name: 'Introduction to Python'}
+    { Week: '2', name: 'Data Abstraction in Python'}
+    { Week: '3', name: 'Creative Development Sprint'}
+    { Week: '4', name: 'Python Web Server Project'}
+    { Week: '5', name: 'UI Starters'}
+    { Week: '6', name: 'Deployment'}
+
+];
