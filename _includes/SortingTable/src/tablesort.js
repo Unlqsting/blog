f071e9d3760fe6f6a10e505b93e0f0988cb5f60e
Unlqsting <noorbijapur500@gmail.com>
2022-09-28 21:55:04 -0700

    javahtmltable

--- _includes/SortingTable/src/tablesort.js
+++ _includes/SortingTable/src/tablesort.js
@@ -1,11 +1,29 @@
 let sortDirection = false;
 let tableData = [
-    { Week: '0', name: 'Tool Setup Sprint and Pair Programming'}
-    { Week: '1', name: 'Introduction to Python'}
-    { Week: '2', name: 'Data Abstraction in Python'}
-    { Week: '3', name: 'Creative Development Sprint'}
-    { Week: '4', name: 'Python Web Server Project'}
-    { Week: '5', name: 'UI Starters'}
+    { Week: '0', name: 'Tool Setup Sprint and Pair Programming'},
+    { Week: '1', name: 'Introduction to Python'},
+    { Week: '2', name: 'Data Abstraction in Python'},
+    { Week: '3', name: 'Creative Development Sprint'},
+    { Week: '4', name: 'Python Web Server Project'},
+    { Week: '5', name: 'UI Starters'},
     { Week: '6', name: 'Deployment'}
 
 ];
+
+window.onload = () => {
+
+loadTableData(tableData);
+
+};
+
+function loadTableData(tableData){
+    const tableBody = document.getElementById('tableData');
+    let dataHtml = '';
+
+    for(let data of tableData) {
+        dataHtml += `<tr><td>${week.number}</td><td>${blog.name}</td></tr>`;
+    }
+    console.log(dataHtml)
+
+    tableBody.innerHtml = datahtml;
+}
